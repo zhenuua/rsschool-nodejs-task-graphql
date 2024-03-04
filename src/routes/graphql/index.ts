0a7cf30e23dd187f3628fbd4bf2173b5ca1c8837
Yauheni Zakharenka <47642482+zhenuua@users.noreply.github.com>
2024-03-04 21:32:24 +0300

    init: start grapghql task

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
 
+
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
     url: '/',
